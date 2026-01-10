bb1d9c97398ae9f0bf90d7e04fcd1845d3821135
jd-13 <jackd8989@gmail.com>
2026-01-10 21:08:15 +0000

    Fix colours test

--- test/colours_test.go
+++ test/colours_test.go
@@ -12,8 +12,8 @@ import (
 func TestColours(t *testing.T) {
 	// Read the file
 	content, err := os.ReadFile("../data/colours/v1.json")
-	require.NotEmpty(t, content)
 	require.NoError(t, err)
+	require.NotEmpty(t, content)
 
 	// Read the json
 	var colours map[string]interface{}
@@ -24,16 +24,23 @@ func TestColours(t *testing.T) {
 	require.NotEmpty(t, colours, "colours should not be empty")
 
 	// Validate each colour
-	for colourName := range colours {
-		colourData, ok := colours[colourName].(map[string]interface{})
+	for colourName, colourValue := range colours {
+		colourData, ok := colourValue.(map[string]interface{})
 		require.True(t, ok, "colour %s should be a map", colourName)
 
 		// Validate gender forms
 		for _, gender := range []string{"masculine", "feminine", "neuter", "plural"} {
 			assert.Contains(t, colourData, gender, "colour %s should have %s form", colourName, gender)
-			declension, ok := colourData[gender].(string)
-			require.True(t, ok, "colour %s %s should be a string", colourName, gender)
-			assert.NotEmpty(t, declension, "colour %s %s should not be empty", colourName, gender)
+
+			values, ok := colourData[gender].([]interface{})
+			require.True(t, ok, "colour %s %s should be an array", colourName, gender)
+			require.NotEmpty(t, values, "colour %s %s should not be empty", colourName, gender)
+
+			for _, v := range values {
+				s, ok := v.(string)
+				require.True(t, ok, "colour %s %s entries should be strings", colourName, gender)
+				assert.NotEmpty(t, s, "colour %s %s entries should not be empty", colourName, gender)
+			}
 		}
 	}
 }
